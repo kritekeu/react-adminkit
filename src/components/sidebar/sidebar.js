5e7c35821d59e9165492cf2d3acffc3b6081f3c8
Bohemicus <mancini@kritek.eu>
2022-01-19 19:12:27 +0100

    sidebar fix

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -106,15 +106,7 @@ const Sidebar = (props) => (
                     </ul>
 
                     <div className="sidebar-cta">
-                        <div className="sidebar-cta-content">
-                            <strong className="d-inline-block mb-2">Upgrade to Pro</strong>
-                            <div className="mb-3 text-sm">
-                                Are you looking for more components? Check out our premium version.
-                            </div>
-                            <div className="d-grid">
-                                <a href="upgrade-to-pro.html" className="btn btn-primary">Upgrade to Pro</a>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </nav>
